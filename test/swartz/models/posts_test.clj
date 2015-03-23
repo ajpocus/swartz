2659b45fefca04d5565cdbf32ca1ace0dc81e974
Austin Pocus <austin.pocus@gmail.com>
2015-03-23 17:29:43 -0400

    Test descriptions on user and post tests

--- test/swartz/models/posts_test.clj
+++ test/swartz/models/posts_test.clj
@@ -7,42 +7,56 @@
 (use-fixtures :each clean-db)
 
 (deftest test-create-with-url
-  (let [user (first (users/find-all test-db))
-        user-id (:id user)
-        params (assoc post-params
-                      :content nil
-                      :user-id user-id)
-        post (create-test-post params)]
-    (is (= (:url params) (:url post)))))
+  (testing "Create a post with url only"
+    (let [user (first (users/find-all test-db))
+          user-id (:id user)
+          params (assoc post-params
+                        :content nil
+                        :user-id user-id)
+          post (create-test-post params)]
+      (is (= (:url params) (:url post))))))
 
 (deftest test-create-with-content
-  (let [user (first (users/find-all test-db))
-        user-id (:id user)
-        params (assoc post-params
-                      :url nil
-                      :user-id user-id)
-        post (create-test-post params)]
-    (is (= (:content params) (:content post)))))
+  (testing "Create a post with content only"
+    (let [user (first (users/find-all test-db))
+          user-id (:id user)
+          params (assoc post-params
+                        :url nil
+                        :user-id user-id)
+          post (create-test-post params)]
+      (is (= (:content params) (:content post))))))
+
+(deftest test-create-with-both-url-and-content
+  (testing "Create a post with both content and url"
+    (let [user (first (users/find-all test-db))
+          user-id (:id user)
+          params (assoc post-params
+                        :user-id user-id)
+          post (create-test-post params)]
+      (is (= (:url params) (:url post)))
+      (is (= (:content params) (:content post))))))
 
 (deftest test-create-with-no-url-or-content
-  (let [user (first (users/find-all test-db))
-        user-id (:id user)
-        params (assoc post-params
-                      :url nil
-                      :content nil
-                      :user-id user-id)]
+  (testing "Try creating a post with no url or content"
+    (let [user (first (users/find-all test-db))
+          user-id (:id user)
+          params (assoc post-params
+                        :url nil
+                        :content nil
+                        :user-id user-id)]
+      (try
+        (create-test-post params)
+        (catch Exception e
+          (is (= (class e) org.postgresql.util.PSQLException)))
+        (finally
+          (is (= 0 (count (posts/find-all test-db)))))))))
+
+(deftest test-create-with-no-user-id
+  (testing "Try creating a post with no user id"
     (try
-      (create-test-post params)
+      ;; post-params doesn't have user id by default -- it has to be assoc'd
+      (create-test-post)
       (catch Exception e
         (is (= (class e) org.postgresql.util.PSQLException)))
       (finally
         (is (= 0 (count (posts/find-all test-db))))))))
-
-(deftest test-create-with-no-user-id
-  (try
-    ;; post-params doesn't have user id by default -- it has to be assoc'd
-    (create-test-post)
-    (catch Exception e
-      (is (= (class e) org.postgresql.util.PSQLException)))
-    (finally
-      (is (= 0 (count (posts/find-all test-db)))))))
--- test/swartz/models/users_test.clj
+++ test/swartz/models/users_test.clj
@@ -6,39 +6,45 @@
 (use-fixtures :each clean-db)
 
 (deftest test-create-with-username-and-password
-  (create-test-user)
-  (is (= (:username user-params)
-         (:username (first (users/find-all test-db))))))
+  (testing "Create a user with username and password"
+    (create-test-user)
+    (is (= (:username user-params)
+           (:username (first (users/find-all test-db)))))))
 
 (deftest test-create-with-duplicate-username
-  (create-test-user)
-  (try
+  (testing "Try creating a user with a duplicate username"
     (create-test-user)
-    (catch Exception e
-      (is (= (class e) org.postgresql.util.PSQLException)))
-    (finally
-      (is (= 1 (count (users/find-all test-db)))))))
+    (try
+      (create-test-user)
+      (catch Exception e
+        (is (= (class e) org.postgresql.util.PSQLException)))
+      (finally
+        (is (= 1 (count (users/find-all test-db))))))))
 
 (deftest test-create-with-no-username
-  (try
-    (create-test-user (assoc user-params :username nil))
-    (catch Exception e
-      (is (= (class e) org.postgresql.util.PSQLException)))
-    (finally
-      (is (empty? (users/find-all test-db))))))
+  (testing "Try creating a user with no username"
+    (try
+      (create-test-user (assoc user-params :username nil))
+      (catch Exception e
+        (is (= (class e) org.postgresql.util.PSQLException)))
+      (finally
+        (is (empty? (users/find-all test-db)))))))
 
 (deftest test-find-all
-  (let [user1 (users/create<! test-db "foobar" "password")
-        user2 (users/create<! test-db "bazquux" "1234")]
-    (is (= 2 (count (users/find-all test-db))))))
+  (testing "Finding all users"
+    (let [user1 (create-test-user)
+          user2 (create-test-user (assoc user-params :username "bazquux"))]
+      (is (= 2 (count (users/find-all test-db)))))))
 
 (deftest test-find-by-username
-  (let [username "foo"
-        password "pass"]
-    (users/create<! test-db username password)
-    (is (= 1 (count (users/find-by-username test-db username))))))
+  (testing "Find a user by username"
+    (let [username "foo"
+          password "pass"]
+      (users/create<! test-db username password)
+      (is (= 1 (count (users/find-by-username test-db username)))))))
 
 (deftest test-delete-all
-  (users/create<! test-db "asdf" "qwerty")
-  (users/delete-all! test-db)
-  (is (= 0 (count (users/find-all test-db)))))
+  (testing "Delete all users"
+    (create-test-user "asdf" "qwerty")
+    (users/delete-all! test-db)
+    (is (= 0 (count (users/find-all test-db))))))
