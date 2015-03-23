fb2328821988245e1b5807bacc92ea6ce7ea35fc
Austin Pocus <austin.pocus@gmail.com>
2015-03-23 16:40:07 -0400

    Rearrange post tests to run fixtures every time
    I'd rather write the tests with "testing" blocks, and write a single "deftest" for each function I'm testing. Unfortunately, the clojure.test fixtures will only run for each "deftest" or "with-test" block, so I rearranged the post tests to use "deftest" for each post I create and test.

--- test/swartz/models/posts_test.clj
+++ test/swartz/models/posts_test.clj
@@ -6,37 +6,43 @@
 
 (use-fixtures :each clean-db)
 
-(deftest test-create
+(deftest test-create-with-url
   (let [user (first (users/find-all test-db))
-        user-id (:id user)]
-    (testing "Create with URL"
-      (let [params (assoc post-params
-                          :content nil
-                          :user-id user-id)
-            post (create-test-post params)]
-        (is (= (:url params) (:url post)))))
-    (testing "Create with content"
-      (let [params (assoc post-params
-                          :url nil
-                          :user-id user-id)
-            post (create-test-post params)]
-        (is (= (:content params) (:content post)))))
-    (testing "Create with no content or URL"
-      (let [params (assoc post-params
-                          :url nil
-                          :content nil
-                          :user-id user-id)]
-        (try
-          (create-test-post params)
-          (catch Exception e
-            (is (= (class e) org.postgresql.util.PSQLException)))
-          (finally
-            (is (= 0 (count (posts/find-all test-db))))))))
-    (testing "Create with no user id"
-      (try
-        ;; post-params doesn't have user id by default -- it has to be assoc'd
-        (create-test-post)
-        (catch Exception e
-          (is (= (class e) org.postgresql.util.PSQLException)))
-        (finally
-          (is (= 0 (count (posts/find-all test-db)))))))))
+        user-id (:id user)
+        params (assoc post-params
+                      :content nil
+                      :user-id user-id)
+        post (create-test-post params)]
+    (is (= (:url params) (:url post)))))
+
+(deftest test-create-with-content
+  (let [user (first (users/find-all test-db))
+        user-id (:id user)
+        params (assoc post-params
+                      :url nil
+                      :user-id user-id)
+        post (create-test-post params)]
+    (is (= (:content params) (:content post)))))
+
+(deftest test-create-with-no-url-or-content
+  (let [user (first (users/find-all test-db))
+        user-id (:id user)
+        params (assoc post-params
+                      :url nil
+                      :content nil
+                      :user-id user-id)]
+    (try
+      (create-test-post params)
+      (catch Exception e
+        (is (= (class e) org.postgresql.util.PSQLException)))
+      (finally
+        (is (= 0 (count (posts/find-all test-db))))))))
+
+(deftest test-create-with-no-user-id
+  (try
+    ;; post-params doesn't have user id by default -- it has to be assoc'd
+    (create-test-post)
+    (catch Exception e
+      (is (= (class e) org.postgresql.util.PSQLException)))
+    (finally
+      (is (= 0 (count (posts/find-all test-db)))))))
