7a4dc8beafc82d359a3420c56dddad00a17cd525
Austin Pocus <austin.pocus@gmail.com>
2015-03-26 20:39:39 -0400

    Try replacing "postgres" in heroku's DATABASE_URL

--- src/swartz/models/core.clj
+++ src/swartz/models/core.clj
@@ -1,5 +1,8 @@
 (ns swartz.models.core
-  (:require [environ.core :refer [env]]))
+  (:require [environ.core :refer [env]]
+            [clojure.string :as str]))
 
-(def db {:connection-uri (env :database-url)
+(def db {:connection-uri (str/replace (env :database-url)
+                                      #":postgres:"
+                                      ":postgresql:")
          :classname "org.postgresql.Driver"})
